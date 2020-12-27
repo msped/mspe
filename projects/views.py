71b1f43e76d22c1c840201df7c3c431feaa194f8
Matt <mattsp.edwards@googlemail.com>
2020-12-27 21:13:15 +0000

    added all projects view

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Project, Issue, Task
 
 # Create your views here.
+
+class AllProjects(View):
+    template_name = 'all_projects.html'
+    def get(self, request):
+        projects = Project.objects.all()
+        context = {
+            'projects': projects
+        }
+        return render(request, self.template_name, context)
+        

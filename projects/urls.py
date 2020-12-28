1e5e49a39b44addc0324d48c402cbbc7b0b3e77a
Matt <mattsp.edwards@googlemail.com>
2020-12-28 14:52:53 +0000

    added project view url

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth.decorators import login_required
-from .views import AllProjects
+from .views import AllProjects, ProjectView
 
 urlpatterns = [
     path('', AllProjects.as_view(), name="all_projects"),
+    path('<int:p_id>/', ProjectView.as_view(), name="project_view"),
 ]

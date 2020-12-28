e4711f8b63a8d397cb5b9a9cc22b464fe4096634
Matt <mattsp.edwards@googlemail.com>
2020-12-28 23:23:58 +0000

    added delete task url

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth.decorators import login_required
-from .views import AllProjects, ProjectView, TaskView, IssueView, ToggleTask, TaskEdit
+from .views import AllProjects, ProjectView, TaskView, IssueView, ToggleTask, TaskEdit, DeleteTask
 
 urlpatterns = [
     path('', AllProjects.as_view(), name="all_projects"),
@@ -10,4 +10,5 @@ urlpatterns = [
     path('issue/<int:i_id>/', IssueView.as_view(), name="issue_view"),
     path('task/toggle-task/<int:t_id>', ToggleTask, name="toggle_task"),
     path('task/<int:t_id>/edit', TaskEdit.as_view(), name="task_edit"),
+    path('task/<int:t_id>/delete', DeleteTask, name="delete_task"),
 ]

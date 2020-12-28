9d4f3d25a0b24fbc990ae6239504a656e77b8fa4
Matt <mattsp.edwards@googlemail.com>
2020-12-28 19:54:31 +0000

    added task and toggle urls

--- projects/urls.py
+++ projects/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth.decorators import login_required
-from .views import AllProjects, ProjectView, TaskView, IssueView
+from .views import AllProjects, ProjectView, TaskView, IssueView, ToggleTask, TaskEdit
 
 urlpatterns = [
     path('', AllProjects.as_view(), name="all_projects"),
     path('<int:p_id>/', ProjectView.as_view(), name="project_view"),
     path('task/<int:t_id>/', TaskView.as_view(), name="task_view"),
     path('issue/<int:i_id>/', IssueView.as_view(), name="issue_view"),
+    path('task/toggle-task/<int:t_id>', ToggleTask, name="toggle_task"),
+    path('task/<int:t_id>/edit', TaskEdit.as_view(), name="task_edit"),
 ]

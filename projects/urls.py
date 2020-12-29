9554028f163e0ea76d68c8f7a0a98eec92a26723
Matt <mattsp.edwards@googlemail.com>
2020-12-29 00:28:30 +0000

    added issue toggle url

--- projects/urls.py
+++ projects/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth.decorators import login_required
-from .views import AllProjects, ProjectView, TaskView, IssueView, ToggleTask, TaskEdit, DeleteTask
+from .views import AllProjects, ProjectView, TaskView, IssueView, ToggleTask, TaskEdit, DeleteTask, ToggleIssue
 
 urlpatterns = [
     path('', AllProjects.as_view(), name="all_projects"),
     path('<int:p_id>/', ProjectView.as_view(), name="project_view"),
     path('task/<int:t_id>/', TaskView.as_view(), name="task_view"),
-    path('issue/<int:i_id>/', IssueView.as_view(), name="issue_view"),
     path('task/toggle-task/<int:t_id>', ToggleTask, name="toggle_task"),
     path('task/<int:t_id>/edit', TaskEdit.as_view(), name="task_edit"),
     path('task/<int:t_id>/delete', DeleteTask, name="delete_task"),
+    path('issue/<int:i_id>/', IssueView.as_view(), name="issue_view"),
+    path('issue/toggle-issue/<int:i_id>', ToggleIssue, name="toggle_issue"),
 ]

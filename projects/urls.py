feef672350472760ad11636fc50789c2fccf5074
Matt <mattsp.edwards@googlemail.com>
2020-12-31 00:13:28 +0000

    added login required to backend views

--- projects/urls.py
+++ projects/urls.py
@@ -4,16 +4,16 @@ from django.contrib.auth.decorators import login_required
 from .views import AllProjects, ProjectView, TaskView, IssueView, ToggleTask, TaskEdit, DeleteTask, ToggleIssue, IssueEdit, DeleteIssue, AddTask, AddIssue
 
 urlpatterns = [
-    path('', AllProjects.as_view(), name="all_projects"),
-    path('<int:p_id>/', ProjectView.as_view(), name="project_view"),
-    path('<int:p_id>/add/task', AddTask.as_view(), name="add_task"),
-    path('task/<int:t_id>/', TaskView.as_view(), name="task_view"),
+    path('', login_required(AllProjects.as_view()), name="all_projects"),
+    path('<int:p_id>/', login_required(ProjectView.as_view()), name="project_view"),
+    path('<int:p_id>/add/task', login_required(AddTask.as_view()), name="add_task"),
+    path('task/<int:t_id>/', login_required(TaskView.as_view()), name="task_view"),
     path('task/toggle-task/<int:t_id>', ToggleTask, name="toggle_task"),
-    path('task/<int:t_id>/edit', TaskEdit.as_view(), name="task_edit"),
+    path('task/<int:t_id>/edit', login_required(TaskEdit.as_view()), name="task_edit"),
     path('task/<int:t_id>/delete', DeleteTask, name="delete_task"),
-    path('issue/<int:i_id>/', IssueView.as_view(), name="issue_view"),
+    path('issue/<int:i_id>/', login_required(IssueView.as_view()), name="issue_view"),
     path('issue/toggle-issue/<int:i_id>', ToggleIssue, name="toggle_issue"),
-    path('issue/<int:i_id>/edit', IssueEdit.as_view(), name="issue_edit"),
+    path('issue/<int:i_id>/edit', login_required(IssueEdit.as_view()), name="issue_edit"),
     path('issue/<int:i_id>/delete', DeleteIssue, name="delete_issue"),
-    path('<int:p_id>/add/issue', AddIssue.as_view(), name="add_issue"),
+    path('<int:p_id>/add/issue', login_required(AddIssue.as_view()), name="add_issue"),
 ]

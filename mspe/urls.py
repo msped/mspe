953d3dddba876bb70fb62fcf442afce5b1232388
Matt <mattsp.edwards@googlemail.com>
2021-03-08 00:16:55 +0000

    added development urls to main project urls

--- mspe/urls.py
+++ mspe/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('dev/', include('development.urls'))
     path('contact/', include('contact.urls')),
     path('projects/', include('projects.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

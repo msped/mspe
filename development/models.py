da727170837d0f717693d204e3619432e311eaa1
Matt <mattsp.edwards@googlemail.com>
2021-03-13 22:17:17 +0000

    added developement model

--- development/models.py
+++ development/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Development(models.Model):
+    """Model for development projects"""
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    url = models.URLField(blank=True, null=True)
+    github_link = models.URLField(blank=True, null=True)
+    created_date = models.DateField()
+
+    def __str__(self):
+        return self.name

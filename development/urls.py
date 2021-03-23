dac519172a130d90fb946bb59554c8ac55d0f088
Matt <mattsp.edwards@googlemail.com>
2021-03-23 23:13:54 +0000

    added get project url

--- development/urls.py
+++ development/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import DevelopmentView
+from .views import DevelopmentView, GetProject
 
 urlpatterns = [
-    path('', DevelopmentView.as_view(), name="development")
+    path('', DevelopmentView.as_view(), name="development"),
+    path('get-project/<int:p_id>', GetProject.as_view(), name="get_project")
 ]

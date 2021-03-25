8ff6b4eeb790dff1fef062de67d556539b625563
Matt <mattsp.edwards@googlemail.com>
2021-03-25 21:24:33 +0000

    added technologies and DevTech model for relationship

--- development/models.py
+++ development/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+class Technologies(models.Model):
+    name = models.CharField(max_length=50)
+    more_info = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
 class Development(models.Model):
     """Model for development projects"""
     name = models.CharField(max_length=50)
@@ -12,3 +19,11 @@ class Development(models.Model):
 
     def __str__(self):
         return self.name
+        
+class DevelopmentTech(models.Model):
+    """Link Tech to Development projects"""
+    developement = models.ForeignKey(Development, on_delete=models.CASCADE)
+    tech = models.ForeignKey(Technologies, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.Developement} - {self.Tech}'

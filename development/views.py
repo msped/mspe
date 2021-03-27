852f3dca6b33e3e399c3df97250c52ebdb89e624
Matt <mattsp.edwards@googlemail.com>
2021-03-27 14:44:55 +0000

    swtched GetProject to a get request

--- development/views.py
+++ development/views.py
@@ -16,10 +16,7 @@ class DevelopmentView(View):
         return render(request, self.template_name, {'devs': dev_projects})
 
 class GetProject(View):
-    @method_decorator(csrf_exempt)
-    def dispatch(self, request, *args, **kwargs):
-        return super(GetProject, self).dispatch(request, *args, **kwargs)
-    def post(self, request, p_id):
+    def get(self, request, p_id):
         dev = Development.objects.get(id=p_id)
         tech = DevelopmentTech.objects.filter(development=dev)
         template = render_to_string('modal_template.html', {'dev': dev, 'tech': tech})

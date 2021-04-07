25db0e38a84f4607e27919f9f34304209a93b468
Matt <mattsp.edwards@googlemail.com>
2021-04-07 21:11:10 +0100

    added home view

--- home/views.py
+++ home/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.views import View
+from development.models import Development
 
 # Create your views here.
 
 class HomeView(View):
     template_name = "home.html"
     def get(self, request):
-        return render(request, self.template_name)
+        latest_project = Development.objects.all()[0]
+        return render(
+            request,
+            self.template_name,
+            {'project': latest_project}
+        )

2f50dd3da1941865d660e6a7bcf95b3eeed87442
Matt Edwards <mattsp.edwards@googlemail.com>
2021-05-22 16:50:25 +0100

    added sitemap to installed apps and site_id

--- mspe/settings.py
+++ mspe/settings.py
@@ -30,6 +30,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
 
 # Application definition
 
@@ -40,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'django_forms_bootstrap',
     'captcha',
     'home',

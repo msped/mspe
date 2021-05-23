db6927f86c733a6f0fc695b12d9ce540781df245
Matt Edwards <mattsp.edwards@googlemail.com>
2021-05-23 18:57:35 +0100

    addeed error handler views

--- errors/views.py
+++ errors/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def handler400View(request, exception):
+    return render(request, "400.html", status=400)
+
+def handler403View(request, exception):
+    return render(request, "403.html", status=403)
+
+def handler404View(request, exception):
+    return render(request, "404.html", status=404)
+
+def handler500View(request, *args, **argv):
+    return render(request, "500.html", status=500)

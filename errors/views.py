700c076c34b5746d7c52d14b7dc283390ce8caf0
Matt Edwards <mattsp.edwards@googlemail.com>
2021-05-29 18:06:59 +0100

    added sntry error capturing for 500 pages

--- errors/views.py
+++ errors/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from sentry_sdk import last_event_id
 
 # Create your views here.
 
@@ -12,4 +13,6 @@ def handler404View(request, exception):
     return render(request, "404.html", status=404)
 
 def handler500View(request, *args, **argv):
-    return render(request, "500.html", status=500)
+     return render(request, "500.html", {
+        'sentry_event_id': last_event_id(),
+    }, status=500)

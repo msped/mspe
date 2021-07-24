7a99ec7791ae0108fbe6c32f059426ff19896216
Matt Edwards <mattsp.edwards@googlemail.com>
2021-07-24 18:12:50 +0100

    renamed resume to cv in line with app name

--- onlinecv/urls.py
+++ onlinecv/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="resume.html"), name="resume")
+    path('', TemplateView.as_view(template_name="cv.html"), name="cv")
 ]

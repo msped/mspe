63f9a6df2014c841745d55a2e79519e577097935
Matt Edwards <mattsp.edwards@googlemail.com>
2022-01-06 19:00:33 +0000

    feat: added is_active to verification model

--- onlinecv/models.py
+++ onlinecv/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Verification(models.Model):
     code = models.CharField(max_length=20)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.code

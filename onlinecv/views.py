c846aae4f1ddcd6332c5dab177625a497f98cf93
Matt Edwards <mattsp.edwards@googlemail.com>
2022-01-06 19:05:00 +0000

    feat: added view for onlinecv

--- onlinecv/views.py
+++ onlinecv/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
+
+from .models import Verification
 
 # Create your views here.
+
+class OnlineCV(View):
+    template_name = "cv.html"
+
+    def get(self, request):
+        verification_code = Verification.objects.get(is_active=True)
+        return render(request, self.template_name, {'code': verification_code})

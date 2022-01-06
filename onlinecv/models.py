2add1d99f8ad0c581313e850fcb050d015e05cf9
Matt Edwards <mattsp.edwards@googlemail.com>
2022-01-06 18:51:31 +0000

    feat: added verification models

--- onlinecv/models.py
+++ onlinecv/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Verification(models.Model):
+    code = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.code

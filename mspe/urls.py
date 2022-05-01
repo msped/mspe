22a973ab821e08f24cf83416c223a3c71e7378bc
Matt Edwards <mattsp.edwards@googlemail.com>
2022-05-01 19:44:03 +0100

    feat: removed sitemap import

--- mspe/urls.py
+++ mspe/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.sitemaps.views import sitemap
 from django.views.generic.base import TemplateView
 
 urlpatterns = [

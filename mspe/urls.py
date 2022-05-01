5dec67ea1d6c6265c1409be6dd8c8e8d114b3de7
Matt Edwards <mattsp.edwards@googlemail.com>
2022-05-01 19:46:19 +0100

    refactor: changed project urls to show api prefix removed projects and robots.txt

--- mspe/urls.py
+++ mspe/urls.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dev/', include('development.urls')),
-    path('cv/', include('onlinecv.urls')),
-    path('contact/', include('contact.urls')),
-    path('projects/', include('projects.urls')),
-    path(
-        "robots.txt",
-        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
-    ),
+    path('api/dev/', include('development.urls')),
+    path('api/cv/', include('onlinecv.urls')),
+    path('api/contact/', include('contact.urls')),
 ]

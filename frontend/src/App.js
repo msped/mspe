a3c09f7ba34a8579d433fd964867afe3b2945705
Matt Edwards <mattsp.edwards@googlemail.com>
2022-05-03 00:59:43 +0100

    feat: added themeprovider header and footer to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
+import { Route, Routes } from 'react-router-dom'
+
+import CustomTheme from './theme';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={CustomTheme()}>
+      <CssBaseline />
+      <Header />
+      <Routes>
+
+      </Routes>
+      <Footer />
+    </ThemeProvider>
   );
 }
 

84338a06df5e46a9cc08dedbc48e330258aa11cd
Matt Edwards <mattsp.edwards@googlemail.com>
2022-05-02 18:59:25 +0100

    refactor: changed view to handle post request

--- contact/views.py
+++ contact/views.py
@@ -1,27 +1,17 @@
-from django.shortcuts import render
-from django.views.generic import View
-from django.http import HttpResponse
-from django.template.loader import render_to_string
-from django.contrib import messages
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
 from django.core.mail import send_mail
-from .forms import ContactForm
+from .serializers import FormSerializer
 
-# Create your views here.
-
-class ContactView(View):
-    """CBV for contact page"""
-    template_name = "contact.html"
-    def get(self, request):
-        context = {'form': ContactForm}
-        return render(request, self.template_name, context)
-
+class SendMessage(APIView):
     def post(self, request):
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data['name']
-            email = form.cleaned_data['email']
-            subject = form.cleaned_data['subject']
-            message = form.cleaned_data['message']
+        serializer = FormSerializer(data=request.data)
+        if serializer.is_valid():
+            name = serializer.data['name']
+            email = serializer.data['email']
+            subject = serializer.data['subject']
+            message = serializer.data['message']
             message += f'\n \n \n From: {name} \n Email: {email}'
             send_mail(
                 subject=subject,
@@ -30,25 +20,5 @@ class ContactView(View):
                 recipient_list=['matt@mspe.me',],
                 fail_silently=False
             )
-            template = render_to_string('message_sent.html')
-            
-        else:
-            name = form['name'].value()
-            email = form['email'].value()
-            subject = form['subject'].value()
-            message = form['message'].value()
-            template = render_to_string(
-                'form_with_errors.html',
-                request=request,
-                context={
-                    'form': ContactForm(initial={
-                        'name': name,
-                        'email': email,
-                        'subject': subject,
-                        'message': message
-                    }),
-                    'form': form
-                }
-            )
-
-        return HttpResponse(template)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

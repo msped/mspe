3c668cd870fb5a0ee4cd33c6fe4b90062ac4664d
Matt Edwards <mattsp.edwards@googlemail.com>
2022-05-02 19:08:37 +0100

    refactor: removed if statement for templates as not being used

--- mspe/settings.py
+++ mspe/settings.py
@@ -58,11 +58,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'mspe.urls'
 
-if DEBUG:
-    TEMPLATES = [
+
+TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -74,27 +74,6 @@ if DEBUG:
         },
     },
 ]
-else:
-    TEMPLATES = [
-        {
-            'BACKEND': 'django.template.backends.django.DjangoTemplates',
-            'DIRS': [],
-            'OPTIONS': {
-                'context_processors': [
-                    'django.template.context_processors.debug',
-                    'django.template.context_processors.request',
-                    'django.contrib.auth.context_processors.auth',
-                    'django.contrib.messages.context_processors.messages',
-                ],
-                'loaders': [
-                ('django.template.loaders.cached.Loader', [
-                    'django.template.loaders.filesystem.Loader',
-                    'django.template.loaders.app_directories.Loader',
-                ]),
-            ],
-            },
-        },
-    ]
 
 WSGI_APPLICATION = 'mspe.wsgi.application'
 

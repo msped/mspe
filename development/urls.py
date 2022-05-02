2b4acc48b7c243ebce53e7cdb2c58f330dd11538
Matt Edwards <mattsp.edwards@googlemail.com>
2022-05-02 01:01:28 +0100

    refactor: changed getproject url path to use primary key

--- development/urls.py
+++ development/urls.py
@@ -3,5 +3,5 @@ from .views import DevelopmentView, GetProject
 
 urlpatterns = [
     path('', DevelopmentView.as_view(), name="development"),
-    path('get-project/<int:p_id>', GetProject.as_view(), name="get_project")
+    path('<pk>/', GetProject.as_view(), name="get_project")
 ]

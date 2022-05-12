c320244a5e2149e4b107fe8fee66302cb9a484ad
Matt Edwards <mattsp.edwards@googlemail.com>
2022-05-12 21:22:13 +0100

    feat: added development.js

---
+++ frontend/src/pages/Development.js
@@ -0,0 +1,47 @@
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+import {
+    Container,
+    Typography,
+    Grid,
+    Stack,
+    Box,
+} from '@mui/material'
+import ProjectCard from '../components/ProjectCard'
+
+export default function Development() {
+    const [projects, setProjects] = useState([])
+
+    useEffect(() => {
+        const search = async () => {
+            const { data } = await axios.get(
+                "http://127.0.0.1:8000/api/dev/"
+            );
+            setProjects(data)
+        };
+        search();
+    }, []);
+
+    return (
+        <Container maxWidth="xl">
+            <Box my={5}>
+                <Stack direction="column" gap={1} alignItems="center">
+                    <Typography color="text" variant="h2" component="h1" sx={{ fontWeight: 500 }}>
+                        Development
+                    </Typography>
+                    <Typography color="text" variant='body1' fontWeight={500}>
+                        Here you can see all my development projects, click on them to find out more!
+                    </Typography>
+                </Stack>
+            </Box>
+            
+            <Grid container spacing={2}>
+                {projects.map((project) => (
+                    <Grid item xs={12} sm={4} key={project.id}>
+                        <ProjectCard project={project} />
+                    </Grid>
+                ))}
+            </Grid>
+        </Container>
+    )
+}

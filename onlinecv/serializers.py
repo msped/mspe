85d3728f8fd8d4bd3221276537274280e2766821
Matt Edwards <mattsp.edwards@googlemail.com>
2022-05-16 23:06:11 +0100

    feat: added verificationcode serialzier

---
+++ onlinecv/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Verification
+
+class VerificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Verification
+        fields = [
+            'code'
+        ]

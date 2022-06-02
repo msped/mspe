61247f90ff1d495f27e5435dcd7bcb71135c38f1
Matt Edwards <mattsp.edwards@googlemail.com>
2022-06-02 17:00:01 +0100

    feat: created resume model

--- onlinecv/models.py
+++ onlinecv/models.py
@@ -8,3 +8,9 @@ class Verification(models.Model):
 
     def __str__(self):
         return self.code
+
+class Resume(models.Model):
+    file = models.FileField(upload_to='cv')
+
+    def __str__(self):
+        return self.file.name

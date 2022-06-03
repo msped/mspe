fe6debf145c33355e7a2d0db7ebeb40efa413e54
Matt Edwards <mattsp.edwards@googlemail.com>
2022-06-03 19:26:08 +0100

    feat: added download CV url path

--- onlinecv/urls.py
+++ onlinecv/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import VerificationCode
+from .views import VerificationCode, DownloadResume
 
 urlpatterns = [
-    path('', VerificationCode.as_view(), name="cv")
+    path('', VerificationCode.as_view(), name="cv"),
+    path('download/', DownloadResume.as_view(), name="download"),
 ]

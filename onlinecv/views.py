18d0cbb7d2c5017b7f72d424f4095292da5babca
Matt Edwards <mattsp.edwards@googlemail.com>
2022-06-03 19:25:58 +0100

    feat: added download CV view

--- onlinecv/views.py
+++ onlinecv/views.py
@@ -1,7 +1,9 @@
 from rest_framework.generics import RetrieveAPIView
+from rest_framework.views import APIView
+from django.http import FileResponse
 
 from .serializers import VerificationSerializer
-from .models import Verification
+from .models import Verification, Resume
 
 # Create your views here.
 
@@ -10,3 +12,12 @@ class VerificationCode(RetrieveAPIView):
 
     def get_object(self):
         return Verification.objects.get(is_active=True)
+
+class DownloadResume(APIView):
+    def get(self, request):
+        queryset = Resume.objects.get(is_active=True)
+        file_handle = queryset.file.open()
+        response = FileResponse(file_handle, content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename=CV.pdf'
+        response['Content-Length'] = queryset.file.size
+        return response

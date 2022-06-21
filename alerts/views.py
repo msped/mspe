4f83daa573f2ac9bc8113f42bfefc87e29942c7d
Matt Edwards <mattsp.edwards@googlemail.com>
2022-06-21 19:42:52 +0100

    feat: added alert view to get views

--- alerts/views.py
+++ alerts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import AlertSerializer
+from .models import Alert
 
 # Create your views here.
+
+class Alerts(APIView):
+    def get(self, request):
+        alert = Alert.objects.filter(active=True)
+        if alert:
+            serializer = AlertSerializer(alert, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_204_NO_CONTENT)

5b95880e0e97a97fefc5779fd1685528ffd2d9cd
Matt Edwards <mattsp.edwards@googlemail.com>
2022-06-21 15:10:53 +0100

    feat: added alert model

--- alerts/models.py
+++ alerts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Alert(models.Model):
+    class SeverityChoices(models.TextChoices):
+        """Options for Alerts"""
+        ERROR = "1", "error"
+        WARNING = "2", "warning"
+        INFO = "3", "info"
+        SUCCESS = "4", "success"
+
+    message = models.CharField(max_length=255)
+    severity = models.CharField(
+        max_length=8, 
+        choices=SeverityChoices.choices, 
+        default="1"
+    )
+
+    def __str__(self):
+        return f'{self.severity} - {self.message}'

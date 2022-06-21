70e812584ec150dadb7524803c686ee9f0c20f24
Matt Edwards <mattsp.edwards@googlemail.com>
2022-06-21 15:21:04 +0100

    feat: added alerts url

--- mspe/urls.py
+++ mspe/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/alerts/', include('alerts.urls')),
     path('api/dev/', include('development.urls')),
     path('api/cv/', include('onlinecv.urls')),
     path('api/contact/', include('contact.urls')),

3f4640225d1ba95816bd9a973df643177ab05b05
Matt Edwards <mattsp.edwards@googlemail.com>
2022-06-24 23:26:58 +0100

    refactor: changed header text to link

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,6 +4,7 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import Link from '@mui/material/Link'
 
 import Sidebar from '../components/Sidebar'
 
@@ -13,7 +14,13 @@ export default function Header() {
       <AppBar position="static" color='inherit'>
         <Toolbar id="back-to-top-anchor">
           <Sidebar />
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: '#fff' }}>
+          <Typography 
+            variant="h6" 
+            component={Link} 
+            sx={{ flexGrow: 1, color: '#fff' }}
+            underline="none"
+            href="/"
+          >
             Matt Edwards
           </Typography>
           <Button

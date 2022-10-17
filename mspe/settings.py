f287e60cbc5b15381d3abe9296e3d54c3e7ebcef
Matt Edwards <mattsp.edwards@googlemail.com>
2022-10-17 22:53:08 +0100

    fix: remvoed unused code

--- mspe/settings.py
+++ mspe/settings.py
@@ -180,7 +180,6 @@ if not DEBUG:
     SECURE_SSL_REDIRECT = True
     SECURE_CONTENT_TYPE_NOSNIFF = True
     SECURE_BROWSER_XSS_FILTER = True
-    [('Matt', os.environ.get('EMAIL_HOST_USER')),]
 else:
     STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 

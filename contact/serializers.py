a01cf7267a87511e3157dc7cc39f34e2d791fa5f
Matt Edwards <mattsp.edwards@googlemail.com>
2022-10-17 22:30:34 +0100

    refactor: removed contact page for messaging via linkedIn

--- contact/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-
-class FormSerializer(serializers.Serializer):
-    name = serializers.CharField()
-    email = serializers.EmailField()
-    subject = serializers.CharField()
-    message = serializers.CharField()

87d9b6a3c233389c5dddb9f2bc5cbbf3c3ea79a6
Matt Edwards <mattsp.edwards@googlemail.com>
2022-11-20 01:14:50 +0000

    refactor: mui import and remove live links due to heroku free ending

--- frontend/src/components/DevelopmentProjects.js
+++ frontend/src/components/DevelopmentProjects.js
@@ -1,13 +1,9 @@
-import React from 'react'
-import Grid from '@mui/material/Grid'
-import Stack from '@mui/material/Stack'
-import Typography from '@mui/material/Typography'
-import Link from '@mui/material/Link'
-import Box from '@mui/material/Box'
+import React from "react";
+import { Grid, Stack, Typography, Link, Box } from "@mui/material";
 
 export default function DevelopmentProjects() {
     return (
-        <Grid container item xs={12} spacing={3} sx={{ margin: 2 }}>
+        <Grid container item xs={12} spacing={3} m={2}>
             <Grid item xs={12}>
                 <Box sx={{ textAlign: "center" }}>
                     <Typography fontWeight={500} variant="h4" component="div">
@@ -17,37 +13,34 @@ export default function DevelopmentProjects() {
             </Grid>
             <Grid item xs={12} md={4}>
                 <Stack direction="column" gap={2}>
-                    <Typography sx={{ textDecoration: 'underline'}}>
+                    <Typography sx={{ textDecoration: "underline" }}>
                         AutoSkunkWorks
                     </Typography>
                     <Typography>
-                        Technologies: Ubuntu Server, Linux, Nginx, Postgres, Django, Bootstrap, 
-                        HTML, CSS, JavaScript (jQuery)
+                        Technologies: Ubuntu Server, Linux, Nginx, Postgres,
+                        Django, Bootstrap, HTML, CSS, JavaScript (jQuery)
                     </Typography>
                     <ul>
                         <li>
-                            A Django and live version of <Link 
-                                href="https://github.com/msped/AutoLog" 
-                                underline='hover'
-                                target="_blank">
+                            A Django and live version of{" "}
+                            <Link
+                                href="https://github.com/msped/AutoLog"
+                                underline="hover"
+                                target="_blank"
+                            >
                                 AutoLog
-                            </Link> deployed on a DigitalOcean Ubuntu Server.
+                            </Link>{" "}
+                            deployed on a DigitalOcean Ubuntu Server for the
+                            management of project car parts.
                         </li>
                     </ul>
                     <Stack direction="row" gap={2}>
-                        <Link 
-                            href="https://github.com/msped/autoskunkworks" 
-                            target="_blank" 
-                            underline='hover'
+                        <Link
+                            href="https://github.com/msped/autoskunkworks"
+                            target="_blank"
+                            underline="hover"
                         >
                             Github
-                        </Link>  
-                        <Link 
-                            href="https://autoskunkworks.herokuapp.com/" 
-                            target="_blank" 
-                            underline='hover'
-                        >
-                            Live Site
                         </Link>
                     </Stack>
                 </Stack>
@@ -55,68 +48,57 @@ export default function DevelopmentProjects() {
 
             <Grid item xs={12} md={4}>
                 <Stack direction="column" gap={2}>
-                    <Typography sx={{ textDecoration: 'underline'}}>
+                    <Typography sx={{ textDecoration: "underline" }}>
                         Check the Match
                     </Typography>
                     <Typography>
-                        Technologies: HTML, CSS, React, Football API, React Router DOM, Material UI
+                        Technologies: HTML, CSS, React, Football API, React
+                        Router DOM, Material UI
                     </Typography>
                     <ul>
                         <li>
-                            A React Application to view football statistics without seeing the final score 
-                            of a match.
+                            A React Application to view football statistics
+                            without seeing the final score of a match.
                         </li>
                     </ul>
                     <Stack direction="row" gap={2}>
-                        <Link 
-                            href="https://github.com/msped/checkthematch" 
-                            target="_blank" 
-                            underline='hover'
+                        <Link
+                            href="https://github.com/msped/checkthematch"
+                            target="_blank"
+                            underline="hover"
                         >
                             Github
-                        </Link>  
-                        <Link 
-                            href="https://checkthematch.herokuapp.com/" 
-                            target="_blank" 
-                            underline='hover'
-                        >
-                            Live Site
                         </Link>
                     </Stack>
-                </Stack> 
+                </Stack>
             </Grid>
 
             <Grid item xs={12} md={4}>
                 <Stack direction="column" gap={2}>
-                    <Typography sx={{ textDecoration: 'underline'}}>
+                    <Typography sx={{ textDecoration: "underline" }}>
                         Project Parts
                     </Typography>
                     <Typography>
-                        Technologies: PostgreSQL, Django, HTML, CSS, jQuery, AJAX, Stripe.js, Heroku
+                        Technologies: PostgreSQL, Django, HTML, CSS, jQuery,
+                        AJAX, Stripe.js, Heroku
                     </Typography>
                     <ul>
                         <li>
-                            An online competition for car modifications built in Pythons Django Framework.
+                            An online competition for car modifications built in
+                            Pythons Django Framework.
                         </li>
                     </ul>
                     <Stack direction="row" gap={2}>
-                        <Link 
-                            href="https://github.com/msped/projectparts" 
-                            target="_blank" 
-                            underline='hover'
+                        <Link
+                            href="https://github.com/msped/projectparts"
+                            target="_blank"
+                            underline="hover"
                         >
                             Github
-                        </Link>  
-                        <Link 
-                            href="https://projectparts.herokuapp.com/" 
-                            target="_blank" 
-                            underline='hover'
-                        >
-                            Live Site
                         </Link>
                     </Stack>
-                </Stack>   
+                </Stack>
             </Grid>
         </Grid>
-    )
+    );
 }

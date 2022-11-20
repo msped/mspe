8428938e9befc44cd5ea1bb16fdae5783b046950
Matt Edwards <mattsp.edwards@googlemail.com>
2022-11-20 01:11:04 +0000

    refactor: mui imports

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,37 +1,28 @@
-import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Link from '@mui/material/Link'
+import React from "react";
+import { AppBar, Box, Toolbar, Typography, Button, Link } from "@mui/material";
 
-import Sidebar from '../components/Sidebar'
+import Sidebar from "../components/Sidebar";
 
 export default function Header() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" color='inherit'>
-        <Toolbar id="back-to-top-anchor">
-          <Sidebar />
-          <Typography 
-            variant="h6" 
-            component={Link} 
-            sx={{ flexGrow: 1, color: '#fff' }}
-            underline="none"
-            href="/"
-          >
-            Matt Edwards
-          </Typography>
-          <Button
-            color="primary"
-            href='/cv'
-            variant='contained'
-          >
-            Resume
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static" color="inherit">
+                <Toolbar id="back-to-top-anchor">
+                    <Sidebar />
+                    <Typography
+                        variant="h6"
+                        component={Link}
+                        sx={{ flexGrow: 1, color: "#fff" }}
+                        underline="none"
+                        href="/"
+                    >
+                        Matt Edwards
+                    </Typography>
+                    <Button color="primary" href="/cv" variant="contained">
+                        Resume
+                    </Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
 }

ee2553d2495a1662bd707f5f55c678fa404bffac
Matt Edwards <mattsp.edwards@googlemail.com>
2022-11-20 01:10:29 +0000

    refactor: mui imports, text size and prettier formatting

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,25 +1,21 @@
-import React from 'react'
-import Link from '@mui/material/Link'
-import Typography from '@mui/material/Typography'
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import Stack from '@mui/material/Stack';
+import React from "react";
+import { Link, Typography, AppBar, Toolbar, Stack } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 function Copyright() {
     return (
-        <Typography 
-            variant="body1" 
+        <Typography
+            variant="body1"
             sx={{
-                color: 'link',
-                flexGrow: 1
+                color: "link",
+                flexGrow: 1,
             }}
         >
-            {'© '}
-            <Link sx={{ color: 'link' }} underline='none' href="/">
+            {"© "}
+            <Link sx={{ color: "link" }} underline="none" href="/">
                 Matt Edwards
-            </Link>{' '}
+            </Link>{" "}
             {new Date().getFullYear()}
         </Typography>
     );
@@ -28,35 +24,39 @@ function Copyright() {
 export default function Footer() {
     return (
         <React.Fragment>
-        <AppBar
-            position="fixed"
-            color="inherit"
-            sx={{ top: 'auto', bottom: 0 }}
-        >
-            <Toolbar>
-                {Copyright()}
-                <Stack direction="row" spacing={2} sx={{ marginX: 2 }}>
-                    <Typography sx={{fontSize: 15, whiteSpace:'normal'}}>Want to contact me? Message me on LinkedIn</Typography>
-                    <Link 
-                        href='https://www.linkedin.com/in/mspe/' 
-                        target='_blank' 
-                        color="link"
-                        aria-label='LinkedIn'
-                    >
-                        <LinkedInIcon />
-                    </Link>
-                    <Link 
-                        href='https://github.com/msped' 
-                        target='_blank' 
-                        color="link"
-                        aria-label='GitHub'
-                    >
-                        <GitHubIcon />
-                    </Link>
-                </Stack>
-            </Toolbar>
-        </AppBar>
-        <Toolbar />
+            <AppBar
+                position="fixed"
+                color="inherit"
+                sx={{ top: "auto", bottom: 0 }}
+            >
+                <Toolbar>
+                    {Copyright()}
+                    <Stack direction="row" spacing={2} mx={2}>
+                        <Typography
+                            variant="body1"
+                            sx={{ whiteSpace: "normal" }}
+                        >
+                            Get in touch on LinkedIn
+                        </Typography>
+                        <Link
+                            href="https://www.linkedin.com/in/mspe/"
+                            target="_blank"
+                            color="link"
+                            aria-label="LinkedIn"
+                        >
+                            <LinkedInIcon />
+                        </Link>
+                        <Link
+                            href="https://github.com/msped"
+                            target="_blank"
+                            color="link"
+                            aria-label="GitHub"
+                        >
+                            <GitHubIcon />
+                        </Link>
+                    </Stack>
+                </Toolbar>
+            </AppBar>
         </React.Fragment>
-    )
+    );
 }

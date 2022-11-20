e74ffa6b8bc7a2b7d4650fa3b3479ff90345f549
Matt Edwards <mattsp.edwards@googlemail.com>
2022-11-20 00:47:12 +0000

    refactor: formatting with prettier

--- frontend/src/components/ProjectCard.js
+++ frontend/src/components/ProjectCard.js
@@ -1,60 +1,58 @@
-import React from 'react'
-import {
-  Card,
-  CardContent,
-  Typography,
-  Grid,
-  Box,
-} from '@mui/material'
-import DetailDialog from '../components/DetailDialog'
-import LazyLoad from 'react-lazyload';
-import TextTruncate from 'react-text-truncate'
-import default_img from '../images/default.jpg'
+import React from "react";
+import { Card, CardContent, Typography, Grid, Box } from "@mui/material";
+import DetailDialog from "../components/DetailDialog";
+import LazyLoad from "react-lazyload";
+import TextTruncate from "react-text-truncate";
+import default_img from "../images/default.jpg";
 
 export default function ProjectCard({ project }) {
-  return (
-    <Card>
-      <CardContent>
-        <Grid container spacing={2}>
-          <Grid item xs={4} sm={12} md={4}>
-              <LazyLoad>
-                <Box sx={{ textAlign: 'center' }}>
-                  <img
-                    src={project.logo == null ? default_img : project.logo}
-                    alt="Development Logo"
-                    style={{
-                      height: "auto",
-                      maxWidth: "100%",
-                      borderRadius: 2.5,
-                      maxHeight: "100px"
-                    }}
-                  />
-                </Box>
-              </LazyLoad>
-          </Grid>
-          <Grid container item xs={8} sm={12} md={8} spacing={2}>
-            <Grid item xs={12}>
-              <Typography variant="h5" component="h1">
-                {project.name}
-              </Typography>
-              </Grid>
-            <Grid item xs={12}>
-              <TextTruncate
-                line={1}
-                element="p"
-                truncateText="..."
-                text={project.description}
-                style={{ color: "#CCCCCC", fontSize: 14 }}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <Box sx={{ flexGrow: 1 }}>
-                <DetailDialog project={project} />
-              </Box>
-            </Grid>
-          </Grid>
-        </Grid>
-      </CardContent>
-    </Card>
-  );
+    return (
+        <Card>
+            <CardContent>
+                <Grid container spacing={2}>
+                    <Grid item xs={4} sm={12} md={4}>
+                        <LazyLoad>
+                            <Box sx={{ textAlign: "center" }}>
+                                <img
+                                    src={
+                                        project.logo == null
+                                            ? default_img
+                                            : project.logo
+                                    }
+                                    alt="Development Logo"
+                                    style={{
+                                        height: "auto",
+                                        maxWidth: "100%",
+                                        borderRadius: 2.5,
+                                        maxHeight: "100px",
+                                    }}
+                                />
+                            </Box>
+                        </LazyLoad>
+                    </Grid>
+                    <Grid container item xs={8} sm={12} md={8} spacing={2}>
+                        <Grid item xs={12}>
+                            <Typography variant="h5" component="h1">
+                                {project.name}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextTruncate
+                                line={1}
+                                element="p"
+                                truncateText="..."
+                                text={project.description}
+                                style={{ color: "#CCCCCC", fontSize: 14 }}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Box sx={{ flexGrow: 1 }}>
+                                <DetailDialog project={project} />
+                            </Box>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </CardContent>
+        </Card>
+    );
 }

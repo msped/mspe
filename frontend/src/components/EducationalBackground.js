fb6531b18e6f2731ed950454565423786301cbb2
Matt Edwards <mattsp.edwards@googlemail.com>
2022-11-20 21:13:03 +0000

    feat: added further educational information

--- frontend/src/components/EducationalBackground.js
+++ frontend/src/components/EducationalBackground.js
@@ -8,23 +8,42 @@ export default function EducationalBackground() {
                 <Typography fontWeight={500} variant="h4" component="div">
                     Educational Background
                 </Typography>
-                <Stack direction="column" py={2}>
-                    <Typography variant="h6" component="p">
-                        <strong>
-                            Diploma in Full Stack Software Development - 2020
-                        </strong>
-                    </Typography>
-                    <Typography variant="body1">
-                        Code Insitute, Napier University, Edinburgh, Scotland
-                    </Typography>
-                    <Typography pt={1}>
-                        Code Institutes Software Development course consists of
-                        4 milestone projects after different learning modules.
-                        After learning a new language or frameworks your skills
-                        are put to the test in a milestone projects were I
-                        earned First Class Honours, equivalent to Level 5 on the
-                        European Qualifications Framework.
-                    </Typography>
+                <Stack direction="column" py={2} spacing={2}>
+                    <Box>
+                        <Typography variant="h6" component="p">
+                            <strong>
+                                Diploma in Full Stack Software Development -
+                                2020
+                            </strong>
+                        </Typography>
+                        <Typography variant="body1">
+                            Code Insitute, Napier University, Edinburgh,
+                            Scotland
+                        </Typography>
+                        <Typography pt={1}>
+                            Code Institutes Software Development course consists
+                            of 4 milestone projects after different learning
+                            modules. After learning a new language or frameworks
+                            your skills are put to the test in a milestone
+                            projects were I earned First Class Honours,
+                            equivalent to Level 5 on the European Qualifications
+                            Framework.
+                        </Typography>
+                    </Box>
+                    <Box>
+                        <Typography variant="h6" component="p">
+                            <strong>
+                                Sir John Deanes College, Northwich - 2013-2015
+                            </strong>
+                        </Typography>
+                        <Typography variant="body1">A-levels</Typography>
+                    </Box>
+                    <Box>
+                        <Typography variant="h6" component="p">
+                            <strong>Weaverham High School - 2008-2013</strong>
+                        </Typography>
+                        <Typography variant="body1">GCCSEs</Typography>
+                    </Box>
                 </Stack>
             </Box>
         </Grid>

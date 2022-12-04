330fa9b473673d70da971f1b9af012feef5d2f17
Matt Edwards <mattsp.edwards@googlemail.com>
2022-12-04 15:59:46 +0000

    feat: added media url to project urls

--- mspe/urls.py
+++ mspe/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/alerts/', include('alerts.urls')),
     path('api/dev/', include('development.urls')),
     path('api/cv/', include('onlinecv.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

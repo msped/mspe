4befdb5abe9c9556c8501d375dfd0dc3f5b10664
Matt Edwards <mattsp.edwards@googlemail.com>
2023-04-09 18:57:14 +0100

    refactor: GetProject and GetLatest CBV's

--- development/views.py
+++ development/views.py
@@ -2,6 +2,8 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
+
 from .serializers import DevelopmentListSerializer
 from .models import Development
 
@@ -14,8 +16,12 @@ class GetProject(RetrieveAPIView):
     serializer_class = DevelopmentListSerializer
     look_up_field = 'id'
 
+    def get_object(self):
+        obj = get_object_or_404(self.get_queryset(), id=self.kwargs[self.lookup_field])
+        return obj
+
 class GetLatest(APIView):
     def get(self, request):
-        latest_project = Development.objects.all()[0]
-        serializer = DevelopmentListSerializer(latest_project, many=False)
+        latest_project = Development.objects.order_by('-created_date').first()
+        serializer = DevelopmentListSerializer(latest_project)
         return Response(serializer.data, status=status.HTTP_200_OK)

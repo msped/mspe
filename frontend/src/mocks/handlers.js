d07122735de61ccd7155ec7dcb552da9edb21e4a
Matt Edwards <mattsp.edwards@googlemail.com>
2023-04-29 22:47:00 +0100

    feat: added latest dev project mock handler

--- frontend/src/mocks/handlers.js
+++ frontend/src/mocks/handlers.js
@@ -23,6 +23,23 @@ export const handlers = [
             ctx.status(200),
             ctx.json({"code": "Hood"})
         )
+    }),
+
+    // Get Latest Project API Mock
+    rest.get(`${apiConstant}/dev/latest`, (req, res, ctx) => {
+        return res(
+            ctx.status(200),
+            ctx.json({
+                "created_date": "23-04-09",
+                "description": "This is a test project.\r\n\r\nIt was made with pain.",
+                "github_link": "https://github.com./msped/shouldiwatchit",
+                "id": 1,
+                "logo": null,
+                "name": "Check the Match",
+                "tech": [],
+                "url": "https://checkthematch.mspe.me/",
+            })
+        )
     })
 
 ]

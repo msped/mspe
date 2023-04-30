ec354a3b52b529dcdaec2ca695999d8285499e4d
Matt Edwards <mattsp.edwards@googlemail.com>
2023-04-30 19:14:13 +0100

    test: refactored Alert test due to error with current mock api call setup

--- frontend/src/__tests__/components/Alerts.test.js
+++ frontend/src/__tests__/components/Alerts.test.js
@@ -1,20 +1,13 @@
 import React from "react";
-import { render, waitFor, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Alerts from "../../components/Alerts";
 
 describe("Alerts", () => {
-    it("alert shouldn't be in the document before hook call", async () => {
-        render(<Alerts />);
-        expect(screen.queryByText("Hello user!")).toBeNull();
-    });
-
     it("should display alerts when there are alerts", async () => {
         render(<Alerts />);
 
-        await waitFor(() =>
-            screen.findByText("Hello user!").then(() => {
-                expect(screen.getByText("Hello user!")).toBeInTheDocument();
-            })
-        );
+        await screen.findByText("Hello user!").then(() => {
+            expect(screen.getByText("Hello user!")).toBeInTheDocument();
+        });
     });
 });
